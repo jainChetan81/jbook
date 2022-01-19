602b89a4814599b2c58da397f385d54aec68e7f3
jainChetan81 <jain.cj.chetan@gmail.com>
2022-01-19 10:36:39 +0530

    refactoring

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable no-restricted-globals */
 import Head from "next/head";
 import PropTypes from "prop-types";
-import { FC, ReactNode } from "react";
+import type { FC, ReactNode } from "react";
+
 type LayoutType = {
 	title?: string;
 	keywords?: string;
@@ -9,22 +9,20 @@ type LayoutType = {
 	children: ReactNode;
 };
 
-const Home: FC<LayoutType> = ({ title, keywords, description, children }) => {
-	return (
-		<>
-			<Head>
-				<title>{title}</title>
-				<meta name="description" content={description} />
-				<meta name="keywords" content={keywords} />
-				<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<link rel="manifest" href="manifest.json" />
-			</Head>
-			<main>{children}</main>
-		</>
-	);
-};
+const Home: FC<LayoutType> = ({ title, keywords, description, children }) => (
+	<>
+		<Head>
+			<title>{title}</title>
+			<meta name="description" content={description} />
+			<meta name="keywords" content={keywords} />
+			<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta charSet="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<link rel="manifest" href="manifest.json" />
+		</Head>
+		<main>{children}</main>
+	</>
+);
 Home.defaultProps = {
 	title: "Pokedox App",
 	description: "A gallery of various Pokemons",
--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@
 module.exports = {
 	reactStrictMode: true,
 	swcMinify: true,
+	images: {
+		domains: ["res.imagekit.io", "doodleipsum.com", "res.cloudinary.com", "ik.imagekit.io"],
+		minimumCacheTTL: 3600,
+		disableStaticImages: true,
+	},
 };
